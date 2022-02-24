200fd2e22a938792cfa0810cd5ab4ef83fffe07c
abhishake21 <y.abhishake210@gmail.com>
2022-02-24 21:21:50 +0530

    tailwind-css code added

---
+++ Tailwind-CSS/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./layouts/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
