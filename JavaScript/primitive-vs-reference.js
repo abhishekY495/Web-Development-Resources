bdac606aac4e74a347340561a3cfcb925f425a65
abhishake21 <y.abhishake210@gmail.com>
2022-03-02 22:53:18 +0530

    javascript basics

---
+++ JavaScript/primitive-vs-reference.js
@@ -0,0 +1,23 @@
+// Primitive
+let num1 = 5;
+let num2 = num1;
+
+console.log(num1);
+console.log(num2);
+num1++;
+console.log(num1);
+console.log(num2);
+
+
+console.log('');
+
+
+// Reference
+let a = [1,2,3];
+let b = a;
+
+console.log(a);
+console.log(b);
+a.push(4);
+console.log(a);
+console.log(b);
