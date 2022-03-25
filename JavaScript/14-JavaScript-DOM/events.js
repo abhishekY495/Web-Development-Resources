96e28729ca61a6011a4b9adf74d4f00b61b370a5
abhishake21 <y.abhishake210@gmail.com>
2022-03-25 22:37:21 +0530

    mouse and keyboard events

--- JavaScript/14-JavaScript-DOM/events.js
+++ JavaScript/14-JavaScript-DOM/events.js
@@ -45,10 +45,10 @@ btnInfo2.addEventListener('click', (e) => {
 })
 
 
-// const img = document.querySelector('.image1');
-// img.addEventListener('click', () => {
-//     console.log(img.getAttribute('src')); 
-// })
+const img = document.querySelector('.image1');
+img.addEventListener('click', () => {
+    console.log(img.getAttribute('src')); 
+})
 
 
 // // // // // // // // //
@@ -66,6 +66,95 @@ darkmodebtn.addEventListener('click', function() {
         document.body.className = 'p-3 mb-2 bg-dark text-white';
     }
 });
+// // // // // // // // // 
 
-// const darkmodebtn2 = document.querySelector('#darkmode');
-// darkmodebtn2.
+console.log('');
+console.log('');
+
+
+const btn10 = document.querySelector('.btn10');
+btn10.addEventListener('click',e => {
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.id);
+    console.log(e.target.className);
+    console.log(e.target.classList);
+    console.log(e.type);
+
+    console.log(e.clientX);
+    console.log(e.clientY);
+    
+    console.log(e.offsetX);
+    console.log(e.offsetY);
+    
+    console.log(e.altKey);
+    console.log(e.shiftKey);
+    console.log(e.ctrlKey);
+
+})
+
+
+
+
+
+// // // // // // // //
+
+
+
+const body = document.body;
+body.addEventListener('keypress', e => {
+    console.log(e.key);
+})
+
+
+const btn20 = document.querySelector('.btn20');
+
+function showType(e){
+    e.preventDefault();
+    console.log('Event Type - ', e.type);
+}
+
+// btn20.addEventListener('click', showType);
+// btn20.addEventListener('dblclick', showType);
+// btn20.addEventListener('mousedown', showType);
+btn20.addEventListener('mouseup', showType);
+
+
+
+const box = document.querySelector('#box');
+
+// box.addEventListener('mouseenter',showType);
+// box.addEventListener('mouseleave',showType);
+
+// box.addEventListener('mouseover',showType);
+// box.addEventListener('mouseout',showType);
+
+// box.addEventListener('mousemove',showType);
+
+const output = document.querySelector('.output')
+box.addEventListener('mousemove',(e)=> {
+    output.innerHTML = `<h3>Mouse X - ${e.offsetX}</h3><h3>Mouse Y - ${e.offsetY}</h3>`
+    box.style.background = `rgb(${e.offsetX},${e.offsetY},40)`
+})
+
+// box.addEventListener('')
+
+const input = document.querySelector('input[type=text]');
+
+// input.addEventListener('keydown',showType)
+// input.addEventListener('keyup',showType)
+// input.addEventListener('keypress',showType)
+// input.addEventListener('focus',showType)
+// input.addEventListener('blur',showType)
+// input.addEventListener('copy',showType)
+// input.addEventListener('paste',showType)
+// input.addEventListener('cut',showType)
+// input.addEventListener('input',showType)
+
+
+const form = document.querySelector('form')
+form.addEventListener('submit',showType)
+
+
+
+// // // // // // //
