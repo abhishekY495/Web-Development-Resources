519404777331ea7bca5adc61ca77ad77d942827a
abhishake21 <y.abhishake210@gmail.com>
2022-03-31 00:07:18 +0530

    Promises code

--- JavaScript/15-Asynchronous-JavaScript/promises.js
+++ JavaScript/15-Asynchronous-JavaScript/promises.js
@@ -127,5 +127,110 @@ myPromise3()
     value += 10;
     // return value;
 }).then((value)=>{
-    console.log(value);
+    // console.log(value); // undefined
 });
+
+
+// // // // // // // // //
+
+
+
+const h1 = document.querySelector('.heading1');
+const h2 = document.querySelector('.heading2');
+const h3 = document.querySelector('.heading3');
+const h4 = document.querySelector('.heading4');
+const h5 = document.querySelector('.heading5');
+const h6 = document.querySelector('.heading6');
+const h7 = document.querySelector('.heading7');
+const h8 = document.querySelector('.heading8');
+
+
+
+// using callbacks
+// setTimeout( () => {
+//     h1.textContent = h1.textContent.toUpperCase();
+//     h1.style.color = 'blue';
+//     setTimeout( () => {
+//         h2.textContent = h2.textContent.toUpperCase();
+//         h2.style.color = 'violet';
+//         setTimeout(()=>{
+//             h3.textContent = h3.textContent.toUpperCase();
+//             h3.style.color = 'crimson';
+//             setTimeout(()=>{
+//                 h4.textContent = h4.textContent.toUpperCase();
+//                 h4.style.color = 'lime';
+//                 setTimeout(()=>{
+//                     h5.textContent = h5.textContent.toUpperCase();
+//                     h5.style.color = 'purple';
+//                     setTimeout(()=>{
+//                         h6.textContent = h6.textContent.toUpperCase();
+//                         h6.style.color = 'orange';
+//                         setTimeout(()=>{
+//                             h7.textContent = h7.textContent.toUpperCase();
+//                             h7.style.color = 'pink';
+//                             setTimeout(()=>{
+//                                 h8.textContent = h8.textContent.toUpperCase();
+//                                 h8.style.color = 'aqua';
+//                             }, 1000)
+//                         }, 1000)
+//                     }, 1000)
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+
+
+// using promise
+function changeText(element, color, time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if (element) {
+                element.textContent = element.textContent.toUpperCase();
+                element.style.color = color;
+                resolve();
+            } else {
+                reject(`${element} not found`);
+            }
+        }, time)
+    });
+}
+
+// changeText(hello,'blue',1000)
+//     .then(()=>{
+//     return changeText(hWorld,'violet',1000)
+// }).then(()=>{
+//     return changeText(bye,'crimson',1000)
+// }).then(()=>{
+//     return changeText(bWorld,'lime',1000)
+// })
+
+
+// Above code can also be written as below.
+changeText(h1,'blue',1000)
+    .then(()=>changeText(h2,'violet',1000))
+    .then(()=>changeText(h3,'crimson',1000))
+    .then(()=>changeText(h4,'lime',1000))
+    // .then(()=>changeText(h14,'lime',1000))
+    .then(()=>changeText(h5,'purple',1000))
+    .then(()=>changeText(h6,'orange',1000))
+    .then(()=>changeText(h7,'pink',1000))
+    .then(()=>changeText(h8,'aqua',1000))
+    .catch((error)=>{window.alert(error)})
+
+
+// // // // // // // 
+
+const promise1 = Promise.resolve('Accepted 🤝');
+const promise2 = 10;
+const promise3 = 'Hello World';
+const promise4 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve('Bye World 🌎'), 2000);
+})
+
+Promise.all([promise1,promise2,promise3,promise4])
+    .then(value => console.log(value));
+
+
+
